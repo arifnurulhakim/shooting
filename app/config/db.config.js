ee1fad4e5cd4f786ec367f6d9063ed41264bf59d
arifnurulhakim <arifnhakim23@gmail.com>
2023-08-15 15:27:33 +0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = {
-  HOST: "database-1.caaq62uigts9.ap-southeast-1.rds.amazonaws.com",
-  USER: "postgres",
-  PASSWORD: "rqIGlz5x2iFPArVBLXIx",
-  DB: "postgres",
-  dialect: "postgres",
-  dialectOptions: {
-    idle_in_transaction_session_timeout: 1000
-  },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 1000
-  }
+    HOST: "database-1.caaq62uigts9.ap-southeast-1.rds.amazonaws.com",
+    USER: "postgres",
+    PASSWORD: "rqIGlz5x2iFPArVBLXIx",
+    DB: "postgres",
+    // HOST: "user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com",
+    // USER: "shooting-star-main-db-0566c731c8e780280",
+    // PASSWORD: "DacHg9bCc6NERpPRAtYkNBYkxXPJe9",
+    // DB: "shooting-star-main-db-0566c731c8e780280",
+    dialect: "postgres",
+    dialectOptions: {
+      idle_in_transaction_session_timeout: 1000,
+      
+    },
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 1000
+    }
 };
